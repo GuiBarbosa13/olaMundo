fa9d48ad10242631819eb3b9c245f4fcbfcb4719
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-04-19 14:55:14 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
+
+const pagina = window.location.pathname === "/"? <Inicio/> : <SobreMim/>;
+
+function App() {
+  return pagina;
+}
+
+export default App;
