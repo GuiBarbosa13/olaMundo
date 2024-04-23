65531f461bf3607ff0730bd4947a96714fc58893
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-04-23 18:03:46 -0300

    implementando sessão de posts relacionados nos posts

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -3,30 +3,48 @@ import { useParams } from "react-router-dom";
 import PostModelo from "../../componentes/PostModelo";
 import posts from "../../json/posts.json";
 import Markdown from "react-markdown";
+import NaoEncontrada from '../NaoEncontrada';
+import Banner from '../../componentes/Banner';
+import PostCard from "../../componentes/PostCard";
+import styles from "./Post.module.css"
 
-export default function Post(){
+export default function Post() {
     const parametros = useParams();
-    const post = posts.find((post)=>{
+    const post = posts.find((post) => {
         return post.id === Number(parametros.id);
-    })
-   
-    if(!post){
-        return <h1>POST NÃO ENCONTRADO</h1>
+    });
+    
+    if (!post) {
+        return <NaoEncontrada />
     }
 
+    const postFiltrado = posts.filter(post => post.id !== Number(parametros.id)).sort((a,b)=>b.id - a.id).slice(0,4);
+
     return (
-        <PostModelo titulo={post.titulo} fotoCapa={`/posts/${post.id}/capa.png`}>
-            <div className="post-markdown-container">
-                <Markdown>
-                    {post.texto}
-                </Markdown>
-            </div>             
-        </PostModelo>
+        <>
+            <Banner/>
+            <PostModelo titulo={post.titulo} fotoCapa={`/posts/${post.id}/capa.png`}>
+                <div className="post-markdown-container">
+                    <Markdown>
+                        {post.texto}
+                    </Markdown>
+                </div>
+                <h2 className={styles.tituloOutrosPosts}>Outros posts que você pode gostar:</h2>
+                <div className={styles.postsRecomendados}>
+                    {
+                        postFiltrado.map((postagem)=>{return <PostCard post={postagem}/>})
+                    }
+                </div>
+                
+
+            </PostModelo>
+        </>
+
     )
 }
 
-    // const postagem = posts.map(post=>post)
+// const postagem = posts.map(post=>post)
 
-    // const titulo = postagem[parametros.id-1].titulo;
-    // const foto = postagem[parametros.id-1].id;
-    // const texto = postagem[parametros.id-1].texto;  
+// const titulo = postagem[parametros.id-1].titulo;
+// const foto = postagem[parametros.id-1].id;
+// const texto = postagem[parametros.id-1].texto;  
