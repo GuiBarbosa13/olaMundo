bc6448929c9d7e0e3f0a0001f4e044c3d036de8a
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-04-23 10:03:10 -0300

    implementando as páginas de posts

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -2,14 +2,17 @@ import { Link } from 'react-router-dom';
 import styles from './PostCard.module.css';
 
 
-export default function PostCard({post}){
-    return(
-        <div className={styles.post}>
-            <img src={`/posts/${post.id}/capa.png`} className={styles.capa} alt='Imagem de capa do post'/>
-            <h1 className={styles.titulo}>
-                {post.titulo}
-            </h1>
-            <Link className={styles.botaoLer}>Ler</Link>
-        </div>
+export default function PostCard({ post }) {
+    return (
+        <Link to={`/posts/${post.id}`}>
+            <div className={styles.post}>
+                <img src={`/posts/${post.id}/capa.png`} className={styles.capa} alt='Imagem de capa do post' />
+                <h1 className={styles.titulo}>
+                    {post.titulo}
+                </h1>
+                <button className={styles.botaoLer}>Ler</button>
+            </div>
+        </Link>
+
     )
 }
--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,7 +1,31 @@
-import { useParams } from "react-router-dom"
+import './Post.css';
+import { useParams } from "react-router-dom";
+import PostModelo from "../../componentes/PostModelo";
+import posts from "../../json/posts.json";
+import Markdown from "react-markdown";
 
 export default function Post(){
     const parametros = useParams();
+    const post = posts.find((post)=>{
+        return post.id === Number(parametros.id);
+    })
 
-    return "Post"
+    console.log(post);
+    
+
+    return (
+        <PostModelo titulo={post.titulo} fotoCapa={`/posts/${post.id}/capa.png`}>
+            <div className="post-markdown-container">
+                <Markdown>
+                    {post.texto}
+                </Markdown>
+            </div>             
+        </PostModelo>
+    )
 }
+
+    // const postagem = posts.map(post=>post)
+
+    // const titulo = postagem[parametros.id-1].titulo;
+    // const foto = postagem[parametros.id-1].id;
+    // const texto = postagem[parametros.id-1].texto;  
